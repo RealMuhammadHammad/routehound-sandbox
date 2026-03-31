4234c174e9515c432c7ef65edc253b5f03960189
Muhammad Hammad <muhammadhammad.biz@gmail.com>
2026-03-31 15:58:21 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+// TODO: Move this to environment variables before launch!
+const GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef123456";
+
+function connectToRepo() {
+  console.log("Authenticating with token: " + GITHUB_TOKEN);
+}
